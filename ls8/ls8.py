3e8d9a5a585c5a3aa672b21cb1364be59511db66
Dan Morse <djmthedev@gmail.com>
2020-05-18 14:24:19 -0700

    added functionality to take a program file as an argument

--- ls8/ls8.py
+++ ls8/ls8.py
@@ -3,9 +3,16 @@
 """Main."""
 
 import sys
+from sys import argv
 from cpu import *
 
 cpu = CPU()
 
-cpu.load()
+if len(argv) < 2:
+    print("Please indicate a program file")
+    exit(1)
+
+_, file = argv
+
+cpu.load(file)
 cpu.run()
