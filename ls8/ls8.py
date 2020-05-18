266c0310ae26761482c292f63eaaf0f6f3fafecc
Dan Morse <djmthedev@gmail.com>
2020-05-18 15:30:00 -0700

    comments in ls8.py

--- ls8/ls8.py
+++ ls8/ls8.py
@@ -6,13 +6,13 @@ import sys
 from sys import argv
 from cpu import *
 
+# print("here")
 cpu = CPU()
-
 if len(argv) < 2:
     print("Please indicate a program file")
     exit(1)
 
 _, file = argv
-
+# print("File:", file)
 cpu.load(file)
 cpu.run()
